8f66b39ec7ddbe207d481b4799c9551029b1b51c
Marcel <marcelkaemper862@gmail.com>
2019-08-10 18:27:35 +0200

    Added homepage

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
+var fs = require('fs');
+var formidable = require('formidable');
+var mv = require('mv');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Cloud' });
+	if(req.session.loggedIn){
+		res.render('index', { title: 'Cloud', user: req.session.user});
+	}else{
+		res.redirect('/login');
+	}
 });
 
-router.post('/login', (req, res, next) => {
-	res.redirect('/');
+router.post("/upload", (req,res,next) => {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function (err, fields, files) {
+		let oldPath = files.filetoupload.path;
+		let path = __dirname+"/"+files.filetoupload.name;
+		mv(oldPath, path, (err) => {
+			if (err) throw err;
+			res.redirect('/');
+		});
+	});
 });
 
 module.exports = router;
